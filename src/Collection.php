a379609b54799ecbfdfa9d1e63addcf38ac34e0a
Andrii Vasyliev <sol@hiqdev.com>
2017-04-12 11:51:53 +0200

    added Collection stub

---
+++ src/Collection.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Composer plugin for config assembling.
+ *
+ * @link      https://github.com/hiqdev/composer-config-plugin
+ * @package   composer-config-plugin
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2016-2017, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hiqdev\composer\config;
+
+/**
+ * Config collection.
+ *
+ * @author Andrii Vasyliev <sol@hiqdev.com>
+ */
+class Collection
+{
+    /// TODO loadConfigs, buildConfig, writeConfig goes here from Builder
+}
