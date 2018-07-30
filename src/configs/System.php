1d526091378a731455018870f0301fe923dbc261
Andrii Vasyliev <sol@hiqdev.com>
2018-07-30 14:09:22 +0000

    Added `System` config `setValue()`, `mergeValues()` and `build()`

--- src/configs/System.php
+++ src/configs/System.php
@@ -12,19 +12,36 @@ namespace hiqdev\composer\config\configs;
 
 /**
  * System class represents system configuration files:
- * __addition, __files.
+ * __files, aliases, extensions.
  *
  * @author Andrii Vasyliev <sol@hiqdev.com>
  */
 class System extends Config
 {
+    public function setValue(string $name, $value)
+    {
+        $this->values[$name] = $value;
+    }
+
     public function setValues(array $values)
     {
         $this->values = $values;
     }
 
+    public function mergeValues(array $values)
+    {
+        $this->values = array_merge($this->values, $values);
+    }
+
     protected function writeFile(string $path, array $data)
     {
         $this->writePhpFile($path, $data, false);
+    }
+
+    public function build()
+    {
+        $this->values = $this->substituteOutputDirs($this->values);
+
+        return $this;
     }
 }
